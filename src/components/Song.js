c84bd66e947203360bbc9174dfc4532ca06aa8de
bruno talarico <bruno.talarico@gmail.com>
2021-02-10 16:36:30 +0000

    created components, initial styling

---
+++ src/components/Song.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Song() {
+  return (
+    <div className="song-container">
+      <h1>Picture</h1>
+      <h1>Songname</h1>
+      <h1>Artist</h1>
+    </div>
+  )
+}
