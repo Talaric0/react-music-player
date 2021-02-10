b9e133c257753333e401aebcf5a2dfde9ad32fb5
bruno talarico <bruno.talarico@gmail.com>
2021-02-10 17:02:30 +0000

    added song and currentsong state and song library data
    Signed-off-by: bruno talarico <bruno.talarico@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //import styles
 import './styles/app.scss'
@@ -7,10 +7,17 @@ import './styles/app.scss'
 import Player from './components/Player'
 import Song from './components/Song'
 
+//import util
+import data from './data'
+
 function App() {
+  //State
+  const [songs, setSongs] = useState(data())
+  const [currentSong, setCurrentSong] = useState(songs[0])
+
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
